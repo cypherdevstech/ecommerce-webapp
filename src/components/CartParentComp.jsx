2dacaf168ab9f449f88da6c85666d76faa997398
shina1 <oswasiu@gmail.com>
2026-07-16 12:39:13 +0100

    implemented add to cart feature

--- src/components/CartParentComp.jsx
+++ src/components/CartParentComp.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
+import { products } from '../utils/products'
 
-const CartParentComp = ({productId}) => {
-  console.log('product id from cart', productId)
-  
+const CartParentComp = ({ productId }) => {
+  const [cartItems, setCartItems] = useState([])
+  useEffect(() => {
+
+    const cartItem = products.find((product) => product?.id == productId);
+    if (cartItem?.id) {
+      setCartItems((prev) => [...prev, cartItem])
+    }
+  }, [productId])
+
+  console.log('product from cart', cartItems)
   return (
-    <aside className='w-full max-w-[30%] bg-white p-5 rounded-2xl h-auto'>
+    <aside className='w-full max-w-[30%] bg-white p-5 rounded-2xl h-auto min-h-[250px]'>
       <div className='w-full flex justify-start items-center'>
-        <h2 className='text-base md:text-2xl text-[#c73a0f] font-semibold'>Your Cart (0)</h2>
+        <h2 className='text-base md:text-2xl text-[#c73a0f] font-semibold'>Your Cart ({cartItems?.length})</h2>
       </div>
       {/* cart content shows here */}
-      <div className='h-atuo hidden'></div>
-      {/* when cart is empty */}
-      <div className='mx-auto flex flex-col gap-3 w-full'>
+      {
+        cartItems?.length > 0 ? (<div className='h-atuo hidden'></div>) : (<div className='mx-auto flex flex-col gap-3 w-full'>
 
-        <div className='w-[200px] h-auto min-h-[200px] mx-auto '>
-          <img src="./images/illustration-empty-cart.svg" alt="Empty cart" className='w-full h-auto object-contain' />
-        </div>
-        <h3 className='text-center text-[#87635a] text-base font-normal'>Your added items will appear here</h3>
-      </div>
+          <div className='w-[200px] h-auto min-h-[200px] mx-auto '>
+            <img src="./images/illustration-empty-cart.svg" alt="Empty cart" className='w-full h-auto object-contain' />
+          </div>
+          <h3 className='text-center text-[#87635a] text-base font-normal'>Your added items will appear here</h3>
+        </div>)
+      }
+
+      {/* when cart is empty */}
+
     </aside>
   )
 }
